790fe27f26b3dae9fdb386239e25598e4bb0f6e4
Alex Sivkov <202493@mail.ru>
2024-03-11 22:33:33 +0400

    added confetti

---
+++ src/table/confetti.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Confetti from 'react-confetti'
+ 
+
+const  confetti = () => {
+   return (
+    <Confetti
+      width={window.innerWidth}
+      height={window.innerWidthdth}
+    />
+  )
+}
+
+export default confetti;
--- src/table/win.js
+++ src/table/win.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Confetti from './confetti';
 
 const Win = ({ wordArray, handleWordArrayClick }) => {
   return (
@@ -7,11 +8,11 @@ const Win = ({ wordArray, handleWordArrayClick }) => {
         <div>
         <div className="gameStatus_under"></div>
         <div className="gameStatus">
+          <Confetti />
           <div className='gameStatus_wrapper'>
-          <h1>ПОБЕДА-А-А-А-АААА!</h1>
+          <h1>ПОБЕДА!</h1>
 				  <div className="btn btn-info m-4" onClick={() => handleWordArrayClick('new')}>Restart</div>
-          </div>
-          
+          </div>          
         </div>
         </div>
       )}
