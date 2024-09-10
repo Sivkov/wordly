796af97617ad42c909d1bc179c73dfa29be29f71
ACE <202493@mail.ru>
2024-09-10 13:29:14 +0300

    fixed keyboard

--- src/table/russianKeyboard.js
+++ src/table/russianKeyboard.js
@@ -1,72 +1,79 @@
 import React from 'react';
 
+
+
 const RussianKeyboard = ({ wordArray, handleWordArrayClick }) => {
-  const russianLetters = Array.from({ length: 32 }, (_, index) => String.fromCharCode(1040 + index));
-  const rClass = (letter) => {
-    let used = []
-    let lClass = 'keyboard-key'
-    wordArray.ATTEMPTS.forEach(element => {
-      if (element.status === 'used') {
-        used = [...used, ...element.word]
-      }
-    });
+	const russianLetters = [
+		["Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ"], // первая строка
+		["Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э"],       // вторая строка
+		["Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю"],                  // третья строка
+	];
+	const rClass = (letter) => {
+		let used = []
+		let lClass = 'keyboard-key'
+		wordArray.ATTEMPTS.forEach(element => {
+			if (element.status === 'used') {
+				used = [...used, ...element.word]
+			}
+		});
 
-    used = [...new Set(used)];
-    //console.log(used)
-
-
-    if (used.includes(letter)) {
-      lClass += ' used'
-      if (wordArray.KEYWORD.includes(letter)) lClass += ' usefull'
-    }
-
-    wordArray.ATTEMPTS.forEach(element => {
-      if (element.status === 'used') {
-        let lIndex = wordArray.KEYWORD.indexOf(letter)
-        if ( lIndex !== -1 && element.word[lIndex] === letter) {
-          lClass += ' usefullOnPlace'
-        }
-      }
-    });
+		used = [...new Set(used)];
+		//console.log(used)
 
 
+		if (used.includes(letter)) {
+			lClass += ' used'
+			if (wordArray.KEYWORD.includes(letter)) lClass += ' usefull'
+		}
 
-    return lClass
-  }
+		wordArray.ATTEMPTS.forEach(element => {
+			if (element.status === 'used') {
+				let lIndex = wordArray.KEYWORD.indexOf(letter)
+				if (lIndex !== -1 && element.word[lIndex] === letter) {
+					lClass += ' usefullOnPlace'
+				}
+			}
+		});
 
-  return (
-    <div>
-      <div className="russian-keyboard">
-        {russianLetters.map((letter, index) => (
-          <button
-            key={index}
-            className={rClass(letter)}
-            onClick={() => handleWordArrayClick('addLetter', letter)}
-          >
-            {letter}
-          </button>
-        ))}
-      </div>
-      <div className='d-flex justify-content-center align-items-center mt-2'>
-        <button
-          className="keyboard-key btn_del"
-          onClick={() => handleWordArrayClick('deleteLetter')}>
-          &#9003;
-        </button>
-        <button
-          className="keyboard-key btn_del"
-          onClick={() => handleWordArrayClick('submit')}>
-          ответ
-        </button>
-        <button
-          className="keyboard-key btn_del"
-          onClick={() => handleWordArrayClick('clue')}>
-          подсказка
-        </button>
-      </div>
-    </div>
+		return lClass
+	}
 
-  );
+	return (
+		<div>
+			<div className="russian-keyboard">
+				{russianLetters.map((letters, ind) => (
+					<div className="lettersLine">
+						{letters.map((letter, index) => (
+							<button
+								key={ind+index}
+								className={rClass(letter)}
+								onClick={() => handleWordArrayClick('addLetter', letter)}>
+								{letter}
+							</button>
+            			))}
+					</div>
+				))}
+			</div>
+			<div className='d-flex justify-content-center align-items-center mt-2'>
+				<button
+					className="keyboard-key btn_del"
+					onClick={() => handleWordArrayClick('deleteLetter')}>
+					&#9003;
+				</button>
+				<button
+					className="keyboard-key btn_del"
+					onClick={() => handleWordArrayClick('submit')}>
+					ответ
+				</button>
+				<button
+					className="keyboard-key btn_del"
+					onClick={() => handleWordArrayClick('clue')}>
+					подсказка
+				</button>
+			</div>
+		</div>
+
+	);
 };
 
 export default RussianKeyboard;
